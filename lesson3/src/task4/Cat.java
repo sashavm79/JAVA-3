9ab3a6b1f16f6c442627bcfc0385a6e889a4b7a7
sashavm79 <sasha_v_m@mail.ru>
2020-04-12 11:45:26 +0300

    Урок 3

---
+++ lesson3/src/task4/Cat.java
@@ -0,0 +1,14 @@
+package task4;
+
+public class Cat {
+    public String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Кот " + name;
+    }
+}
